89779365f8a36cfe7621f6c8a14c3f894bb4577a
Vinicius Lima dos santos <110945541+ViniciusMirtilo@users.noreply.github.com>
2022-12-05 17:37:20 -0300

    Add files via upload

---
+++ necessario/database_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+  // $host = "ads_manha2a.mysql.dbaas.com.br";
+  // $host_user = "ads_manha2a";
+  // $host_password = "AdS22Manha!";
+  // $database_name = "ads_manha2a";
+
+  // $conexao = mysqli_connect($host, $host_user, $host_password, $database_name);
+
+  $host = "ads_manha2a.mysql.dbaas.com.br";
+  $host_user = "ads_manha2a";
+  $host_password = "AdS22Manha!";
+  $database_name = "ads_manha2a";
+
+  $teste = 'mysql:host=ads_manha2a.mysql.dbaas.com.br;dbname=ads_manha2a';
+  $conect = new PDO($teste , $host_user , $host_password);
+
+  $conexao = mysqli_connect($host, $host_user, $host_password, $database_name);
+?>
