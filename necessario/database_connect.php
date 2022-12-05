f16383fffe20300bf6e373d373f56385baaba668
Vinicius Lima dos santos <110945541+ViniciusMirtilo@users.noreply.github.com>
2022-12-05 17:41:41 -0300

    Update database_connect.php

--- necessario/database_connect.php
+++ necessario/database_connect.php
@@ -7,10 +7,10 @@
 
   // $conexao = mysqli_connect($host, $host_user, $host_password, $database_name);
 
-  $host = "ads_manha2a.mysql.dbaas.com.br";
-  $host_user = "ads_manha2a";
-  $host_password = "AdS22Manha!";
-  $database_name = "ads_manha2a";
+  $host = "...";
+  $host_user = "...";
+  $host_password = "...";
+  $database_name = "...";
 
   $teste = 'mysql:host=ads_manha2a.mysql.dbaas.com.br;dbname=ads_manha2a';
   $conect = new PDO($teste , $host_user , $host_password);
